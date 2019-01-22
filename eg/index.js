790915f24e2da81f012d1f1317debcd00b7367b0
Chris Pressey <cpressey@catseye.tc>
2019-01-22 15:51:28 +0000

    Add Javascript to provide example of how to run in a web browser.

---
+++ eg/index.js
@@ -0,0 +1,14 @@
+examplePrograms = [
+    [
+        "chars.zow", 
+        "; Display the Roman alphabet in reverse, in ZOWIE\n; This example source is in the public domain.\n\nMOV R10, 90   ; initially it's \"Z\"\nMOV R1, R1    ; BEGIN TRANSACTION for \"REPEAT\"\nMOV R0, R10   ; output character\nMOV R8, R10   ; decrement character\nMOV R5, 1\nMOV R10, R8\nMOV R8, R10   ; test if character is above \"@\"\nMOV R5, 64\nMOV R3, R8    ; COMMIT AND REPEAT if non-zero\n"
+    ], 
+    [
+        "fact.zow", 
+        "; Compute a factorial, in ZOWIE\n; This example source is in the public domain.\n\n; Expected output is MODIFIER LETTER TRIANGULAR COLON (Unicode character 720).\n; NOTE: this code is also able to properly compute 0! = 1.\n\nMOV R11, 6    ; let's find 6!\nMOV R10, 1    ; accumulator\n\nMOV R8, R11   ; increase multiplicand\nMOV R4, 1\nMOV R11, R8\n\nMOV R1, R1    ; BEGIN TRANSACTION for \"REPEAT\"\n\nMOV R8, R11   ; decrease multiplicand\nMOV R5, 1\nMOV R11, R8\n\nMOV R1, R1    ; BEGIN TRANSACTION for \"IF\"\n\nMOV R8, R10   ; accumulator\nMOV R6, R11   ; multiplied\nMOV R10, R8\n\nMOV R2, R11   ; COMMIT if multiplicand above zero, or ROLLBACK otherwise\nMOV R3, R11   ; COMMIT AND REPEAT if multiplicand above zero\n\nMOV R0, R10   ; output accumulator (as single Unicode character)\n"
+    ], 
+    [
+        "hello.zow", 
+        "; Hello, world! in ZOWIE\n; This example source is in the public domain.\n\nMOV R0, 72\nMOV R0, 101\nMOV R0, 108\nMOV R0, 108\nMOV R0, 111\nMOV R0, 44\nMOV R0, 32\nMOV R0, 119\nMOV R0, 111\nMOV R0, 114\nMOV R0, 108\nMOV R0, 100\nMOV R0, 33\nMOV R0, 10\n"
+    ]
+];
---
+++ eg/launch-zowie.js
@@ -0,0 +1,143 @@
+// Example of running the ZOWIE reference interpreter, zowie.py,
+// under Skulpt, in a web browser.  As of this writing, this
+// can be seen in action here: http://catseye.tc/installation/ZOWIE
+
+function load_file(url, callback)
+{
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function(e) {
+    if (xhr.readyState === 4 && xhr.responseText) {
+      if (xhr.status === 200) {
+        callback(xhr.responseText);
+      } else {
+        alert("Error: could not load " + url + ": " + xhr.statusText);
+      }
+    }
+  };
+  xhr.open("get", url, true);
+  xhr.send();
+}
+
+PresetManager = function() {
+    this.init = function(cfg) {
+        this.selectElem = cfg.selectElem;
+        if (cfg.setPreset) {
+            this.setPreset = cfg.setPreset;
+        }
+        this.clear();
+        var $this = this;
+        this.selectElem.onchange = function() {
+            $this._select(this.options[this.selectedIndex].value);
+        }
+        return this;
+    };
+
+    this.clear = function() {
+        this.reactTo = {};
+        while (this.selectElem.firstChild) {
+            this.selectElem.removeChild(this.selectElem.firstChild);
+        }
+        this.add('(select one...)', function() {});
+        return this;
+    };
+
+    this.add = function(id, callback) {
+        var opt = document.createElement("option");
+        opt.text = id;
+        opt.value = id;
+        this.selectElem.options.add(opt);
+        var $this = this;
+        this.reactTo[id] = callback || this.setPreset;
+        return this;
+    };
+
+    this.setPreset = function(id) {
+        throw new Error("No default setPreset callback configured");
+    };
+
+    this._select = function(id) {
+        this.reactTo[id](id);
+        if (this.onselect) {
+            this.onselect(id);
+        }
+    };
+
+    this.select = function(id) {
+        var i = 0;
+        var opt = this.selectElem.options[i];
+        while (opt) {
+            if (opt.value === id) {
+                this.selectElem.selectedIndex = i;
+                this._select(id);
+                return this;
+            }
+            i++;
+            opt = this.selectElem.options[i];
+        }
+        // if not found, select the "(select one...)" option
+        this.selectElem.selectedIndex = 0;
+        return this;
+    };
+};
+
+function launch(prefix, container, config) {
+  if (typeof container === 'string') {
+    container = document.getElementById(container);
+  }
+  config = config || {};
+
+  document.getElementById('installation').innerHTML =
+    '<div id="control_panel"></div>' +
+    '<button type="button" id="run">Run</button>' +
+    '<span class="control-container">example program: <select id="select_source"></select></span>' +
+    '<pre id="output"></pre>' +
+    '<textarea id="editor" rows="14" cols="60" wrap="off"></textarea>';
+
+  var presetManager = (new PresetManager()).init({
+      selectElem: document.getElementById('select_source')
+  });
+  function makeCallback(sourceText) {
+    return function(id) {
+      document.getElementById('editor').value = sourceText;
+    }
+  }
+  for (var i = 0; i < examplePrograms.length; i++) {
+    presetManager.add(examplePrograms[i][0], makeCallback(examplePrograms[i][1]));
+  }
+  presetManager.select(examplePrograms[0][0]);
+
+  document.getElementById("run").style.enabled = false;
+  var zowie_interpreter;
+
+  load_file("../modules/zowie/src/zowie.py", function(text) {
+      zowie_interpreter = text;
+      document.getElementById("run").style.enabled = true;
+  });
+
+  document.getElementById("run").onclick = function() {
+     var esoprog = document.getElementById("editor").value;
+     var mypre = document.getElementById("output");
+     mypre.innerHTML = '';
+     Sk.canvas = "mycanvas";
+     Sk.pre = "output";
+     Sk.configure({
+         output: function(text) {
+             var mypre = document.getElementById("output");
+             mypre.innerHTML = mypre.innerHTML + text.replace(/\n$/, "");
+         },
+         read: function(x) {
+             if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) {
+                 throw "File not found: '" + x + "'";
+             }
+             return Sk.builtinFiles["files"][x];
+         }
+     });
+     prog = zowie_interpreter + "\n";
+     prog += 'p = Processor()\n';
+     prog += 'p.load_string("""\n';
+     prog += esoprog;
+     prog += '""")\n';
+     prog += 'p.run()\n';
+     eval(Sk.importMainWithBody("<stdin>", false, prog));
+  };
+}
