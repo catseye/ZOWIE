969b88540175f06f53f0bdcb3a8c8428c7c23d1f
Chris Pressey <cpressey@catseye.tc>
2019-01-22 16:20:02 +0000

    Fix indentation, add a missing `var` and a missing semicolon

--- eg/launch-zowie.js
+++ eg/launch-zowie.js
@@ -19,65 +19,58 @@ function load_file(url, callback)
 }
 
 PresetManager = function() {
-    this.init = function(cfg) {
-        this.selectElem = cfg.selectElem;
-        if (cfg.setPreset) {
-            this.setPreset = cfg.setPreset;
-        }
-        this.clear();
-        var $this = this;
-        this.selectElem.onchange = function() {
-            $this._select(this.options[this.selectedIndex].value);
-        }
+  this.init = function(cfg) {
+    this.selectElem = cfg.selectElem;
+    this.clear();
+    var $this = this;
+    this.selectElem.onchange = function() {
+      $this._select(this.options[this.selectedIndex].value);
+    };
+    return this;
+  };
+
+  this.clear = function() {
+    this.reactTo = {};
+    while (this.selectElem.firstChild) {
+      this.selectElem.removeChild(this.selectElem.firstChild);
+    }
+    this.add('(select one...)', function() {});
+    return this;
+  };
+
+  this.add = function(id, callback) {
+    var opt = document.createElement("option");
+    opt.text = id;
+    opt.value = id;
+    this.selectElem.options.add(opt);
+    var $this = this;
+    this.reactTo[id] = callback;
+    return this;
+  };
+
+  this._select = function(id) {
+    this.reactTo[id](id);
+    if (this.onselect) {
+      this.onselect(id);
+    }
+  };
+
+  this.select = function(id) {
+    var i = 0;
+    var opt = this.selectElem.options[i];
+    while (opt) {
+      if (opt.value === id) {
+        this.selectElem.selectedIndex = i;
+        this._select(id);
         return this;
-    };
-
-    this.clear = function() {
-        this.reactTo = {};
-        while (this.selectElem.firstChild) {
-            this.selectElem.removeChild(this.selectElem.firstChild);
-        }
-        this.add('(select one...)', function() {});
-        return this;
-    };
-
-    this.add = function(id, callback) {
-        var opt = document.createElement("option");
-        opt.text = id;
-        opt.value = id;
-        this.selectElem.options.add(opt);
-        var $this = this;
-        this.reactTo[id] = callback || this.setPreset;
-        return this;
-    };
-
-    this.setPreset = function(id) {
-        throw new Error("No default setPreset callback configured");
-    };
-
-    this._select = function(id) {
-        this.reactTo[id](id);
-        if (this.onselect) {
-            this.onselect(id);
-        }
-    };
-
-    this.select = function(id) {
-        var i = 0;
-        var opt = this.selectElem.options[i];
-        while (opt) {
-            if (opt.value === id) {
-                this.selectElem.selectedIndex = i;
-                this._select(id);
-                return this;
-            }
-            i++;
-            opt = this.selectElem.options[i];
-        }
-        // if not found, select the "(select one...)" option
-        this.selectElem.selectedIndex = 0;
-        return this;
-    };
+      }
+      i++;
+      opt = this.selectElem.options[i];
+    }
+    // if not found, select the "(select one...)" option
+    this.selectElem.selectedIndex = 0;
+    return this;
+  };
 };
 
 function launch(prefix, container, config) {
@@ -115,29 +108,30 @@ function launch(prefix, container, config) {
   });
 
   document.getElementById("run").onclick = function() {
-     var esoprog = document.getElementById("editor").value;
-     var mypre = document.getElementById("output");
-     mypre.innerHTML = '';
-     Sk.canvas = "mycanvas";
-     Sk.pre = "output";
-     Sk.configure({
-         output: function(text) {
-             var mypre = document.getElementById("output");
-             mypre.innerHTML = mypre.innerHTML + text.replace(/\n$/, "");
-         },
-         read: function(x) {
-             if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) {
-                 throw "File not found: '" + x + "'";
-             }
-             return Sk.builtinFiles["files"][x];
-         }
-     });
-     prog = zowie_interpreter + "\n";
-     prog += 'p = Processor()\n';
-     prog += 'p.load_string("""\n';
-     prog += esoprog;
-     prog += '""")\n';
-     prog += 'p.run()\n';
-     eval(Sk.importMainWithBody("<stdin>", false, prog));
+    var esoprog = document.getElementById("editor").value;
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = '';
+    // Sk is the Skulpt object, which we assume has been loaded first
+    Sk.canvas = "mycanvas";
+    Sk.pre = "output";
+    Sk.configure({
+      output: function(text) {
+        var mypre = document.getElementById("output");
+        mypre.innerHTML = mypre.innerHTML + text.replace(/\n$/, "");
+      },
+      read: function(x) {
+        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) {
+          throw "File not found: '" + x + "'";
+        }
+        return Sk.builtinFiles["files"][x];
+      }
+    });
+    var prog = zowie_interpreter + "\n";
+    prog += 'p = Processor()\n';
+    prog += 'p.load_string("""\n';
+    prog += esoprog;
+    prog += '""")\n';
+    prog += 'p.run()\n';
+    eval(Sk.importMainWithBody("<stdin>", false, prog));
   };
 }
