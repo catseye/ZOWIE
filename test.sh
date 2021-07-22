fe9d60c12d919b39eadd6a34d256c74953ae8b4b
Chris Pressey <cpressey@catseye.tc>
2021-07-22 19:50:53 +0100

    Run the tests with any of python2, or python3, or zowie-c.

--- test.sh
+++ test.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-APPLIANCES="tests/appliances/zowie.py2.md tests/appliances/zowie.py3.md"
+APPLIANCES=""
+if command -v python2 > /dev/null 2>&1; then
+    APPLIANCES="$APPLIANCES tests/appliances/zowie.py2.md"
+fi
+if command -v python3 > /dev/null 2>&1; then
+    APPLIANCES="$APPLIANCES tests/appliances/zowie.py3.md"
+fi
 if [ -x bin/zowie-c ]; then
     APPLIANCES="$APPLIANCES tests/appliances/zowie-c.md"
+fi
+
+if [ "x$APPLIANCES" = "x" ]; then
+    echo "No suitable Python versions or RPython-compiled executables found."
+    exit 1
 fi
 
 falderal $APPLIANCES tests/ZOWIE.md
